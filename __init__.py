5003a5979bfe095c9cf419608dcf51e65317def1
Yuan-Man <68322456+Yuan-ManX@users.noreply.github.com>
2025-03-10 15:00:13 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,1 +1,11 @@
+from .StyleStudio_nodes import StyleStudio
 
+NODE_CLASS_MAPPINGS = {
+    "StyleStudio Image Stylization": StyleStudio,
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "StyleStudio": "StyleStudio Image Stylization",
+} 
+
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
